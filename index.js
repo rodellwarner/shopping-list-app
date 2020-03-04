0df95707eb7bb94039e60152ca74eb77500b26e5
Rodell Warner <rodell.warner@gmail.com>
2020-03-04 16:37:55 -0600

    add item button works

---
+++ index.js
@@ -0,0 +1,17 @@
+$(function () {
+  $("#js-shopping-list-form").submit(event => {
+    event.preventDefault();
+    const userTextElement = $(event.currentTarget).find("#shopping-list-entry");
+    $(".shopping-list").append(`<li>
+    <span class="shopping-item">${userTextElement.val()}</span>
+    <div class="shopping-item-controls">
+      <button class="shopping-item-toggle">
+        <span class="button-label">check</span>
+      </button>
+      <button class="shopping-item-delete">
+        <span class="button-label">delete</span>
+      </button>
+    </div>
+  </li>`);
+  });
+});
